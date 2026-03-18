e02b4314c0a39243f112a4374f46240b41934032
shaikh22ammar <shaikh22ammar@icloud.com>
2026-03-18 14:43:05 +0530

    Added grid drawing functionality

--- src/main.c
+++ src/main.c
@@ -21,6 +21,9 @@ bool intialize_window(void) {
 	}
 
 	// Creating window with properties
+	const SDL_DisplayMode *display_mode = SDL_GetCurrentDisplayMode(SDL_GetPrimaryDisplay());
+	window_width = display_mode->w;
+	window_height = display_mode->h;
 	SDL_PropertiesID props = SDL_CreateProperties();
 	if (props == 0) {
 		SDL_Log("Unable to create properties: %s", SDL_GetError());
@@ -31,7 +34,7 @@ bool intialize_window(void) {
 	SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_HEIGHT_NUMBER, window_height);
 	SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_X_NUMBER, SDL_WINDOWPOS_CENTERED);
 	SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_Y_NUMBER, SDL_WINDOWPOS_CENTERED);
-	SDL_SetBooleanProperty(props, SDL_PROP_WINDOW_CREATE_BORDERLESS_BOOLEAN, true);
+	SDL_SetBooleanProperty(props, SDL_PROP_WINDOW_CREATE_FULLSCREEN_BOOLEAN, true);
 
 	window = SDL_CreateWindowWithProperties(props);
 	if (window == NULL) {
@@ -50,11 +53,14 @@ bool intialize_window(void) {
 }
 
 bool setup(void) {
+	// Creating raw pixel buffer
 	colorBuffer = (uint32_t *) malloc(sizeof(uint32_t) * window_width * window_height);
 	if (!colorBuffer) {
 		fprintf(stderr, "Failed to allocate memory for color buffer");
 		return false;
 	}
+
+	// Creating buffer texture
 	colorBufferTexture = SDL_CreateTexture(
 		renderer,
 		SDL_PIXELFORMAT_ARGB8888,
@@ -83,6 +89,21 @@ void processInput(void) {
 	}
 }
 
+void drawGrid(int step) {
+	// draw vertical lines
+	for (int j = step - 1; j < window_width; j+= step) {
+		for (int i = 0; i < window_height; i++) {
+			colorBuffer[i*window_width + j] = 0xFFFFFFFF;
+		}
+	}
+	// draw horizontal lines
+	for (int i = step - 1; i < window_height; i+= step) {
+		for (int j = 0; j < window_width; j++) {
+			colorBuffer[i*window_width + j] = 0xFFFFFFFF;
+		}
+	}
+}
+
 void renderColorBuffer(void) {
 	SDL_UpdateTexture(
 		colorBufferTexture,
@@ -105,13 +126,15 @@ void render(void) {
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderClear(renderer);
 	
+	drawGrid(100);
 	renderColorBuffer();
-	clearColorBuffer(0xFFFFFF00);
+	clearColorBuffer(0xFF000000);
 
 	SDL_RenderPresent(renderer);
 }
 
 void destroyWindow(void) {
+	SDL_DestroyTexture(colorBufferTexture);
 	free(colorBuffer);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
