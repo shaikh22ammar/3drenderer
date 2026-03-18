95aa9222fb093b64abdfe40ebcdcd75d75f49daf
shaikh22ammar <shaikh22ammar@icloud.com>
2026-03-18 13:57:25 +0530

    Color buffer

--- src/main.c
+++ src/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
 bool isRunning = false;
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
+uint32_t *colorBuffer = NULL;
+SDL_Texture *colorBufferTexture = NULL;
+int window_width = 800, window_height = 600;
 
 bool intialize_window(void) {
 	// Intialization
@@ -22,8 +27,8 @@ bool intialize_window(void) {
 		return false;
 	}
 	SDL_SetStringProperty(props, SDL_PROP_WINDOW_CREATE_TITLE_STRING, NULL);
-	SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_WIDTH_NUMBER, 800);
-	SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_HEIGHT_NUMBER, 600);
+	SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_WIDTH_NUMBER, window_width);
+	SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_HEIGHT_NUMBER, window_height);
 	SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_X_NUMBER, SDL_WINDOWPOS_CENTERED);
 	SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_Y_NUMBER, SDL_WINDOWPOS_CENTERED);
 	SDL_SetBooleanProperty(props, SDL_PROP_WINDOW_CREATE_BORDERLESS_BOOLEAN, true);
@@ -44,6 +49,26 @@ bool intialize_window(void) {
 	return true;
 }
 
+bool setup(void) {
+	colorBuffer = (uint32_t *) malloc(sizeof(uint32_t) * window_width * window_height);
+	if (!colorBuffer) {
+		fprintf(stderr, "Failed to allocate memory for color buffer");
+		return false;
+	}
+	colorBufferTexture = SDL_CreateTexture(
+		renderer,
+		SDL_PIXELFORMAT_ARGB8888,
+		SDL_TEXTUREACCESS_STREAMING,
+		window_width,
+		window_height
+	);
+	if (!colorBufferTexture) {
+		SDL_Log("Unable to create color buffer texture %s", SDL_GetError());
+		return false;
+	}
+	return true;
+}
+
 void processInput(void) {
 	SDL_Event event;
 	SDL_PollEvent(&event);
@@ -58,17 +83,48 @@ void processInput(void) {
 	}
 }
 
+void renderColorBuffer(void) {
+	SDL_UpdateTexture(
+		colorBufferTexture,
+		NULL,
+		colorBuffer,
+		(int) (window_width * sizeof (uint32_t))
+	);
+	SDL_RenderTexture(renderer, colorBufferTexture, NULL, NULL);
+}
+
+void clearColorBuffer(uint32_t color) {
+	for (int i = 0; i < window_height; i++) {
+		for (int j = 0; j < window_width; j++) {
+			colorBuffer[(i*window_width) + j] = color;
+		}
+	}
+}
+
 void render(void) {
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderClear(renderer);
+	
+	renderColorBuffer();
+	clearColorBuffer(0xFFFFFF00);
+
 	SDL_RenderPresent(renderer);
 }
 
+void destroyWindow(void) {
+	free(colorBuffer);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
 int main() {
-	isRunning = intialize_window();
+	isRunning = intialize_window() && setup();
 	while(isRunning) {
 		processInput();
+		//clearColorBuffer(0xFFFFFF00);
 		render();
 	}
+	destroyWindow();
 	return 0;
 }
