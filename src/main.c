6acf9cee533c4ac6791069c557946f455eee4399
shaikh22ammar <shaikh22ammar@icloud.com>
2026-03-20 23:16:37 +0530

    Cube renders but rotation is broken

--- src/main.c
+++ src/main.c
@@ -8,58 +8,16 @@
 #include "vector.h"
 #include "mesh.h"
 
-// Cube point cloud
-/*
-#define CUBE_DIM 10
-vec3_t cubePointCloud[CUBE_DIM * CUBE_DIM * CUBE_DIM];
-vec3_t cubeStart;
-vec2_t projectedPoints[CUBE_DIM * CUBE_DIM * CUBE_DIM];
-vec3_t cubeRotation = {.x = 0, .y = 0, .z = 0};
-vec3_t cubeCenter;
-float cubeStep;*/
 
-// mesh
+// mesh details
 #define NUM_VERTICES 8
 #define NUM_FACES 12
 mesh_t mesh;
-vec3_t vertices[NUM_VERTICES];
-face_t faces[NUM_FACES];
 
 int previousFrameTime = 0;
 
-/*
-void createCubePointCloud(void) {
-	cubeStart = (vec3_t) {.x = -1, .y = -1, .z = 2};
-	vec3_t currentPoint = cubeStart;
-	cubeStep = 2.0 / (CUBE_DIM - 1);
-	cubeCenter = (vec3_t) {
-		.x = (cubeStart.x + cubeStart.x + cubeStep*CUBE_DIM) * 0.5,
-		.y = (cubeStart.y + cubeStart.y + cubeStep*CUBE_DIM) * 0.5,
-		.z = (cubeStart.z + cubeStart.z + cubeStep*CUBE_DIM) * 0.5
-	};
-	int  zCount = 0;
-	while (zCount < CUBE_DIM) {
-		int yCount = 0;
-		currentPoint.y = cubeStart.y;
-		while (yCount < CUBE_DIM) {
-			int xCount = 0;
-			currentPoint.x = cubeStart.x;
-			while(xCount < CUBE_DIM) {
-				cubePointCloud[zCount*CUBE_DIM*CUBE_DIM + yCount*CUBE_DIM + xCount] = currentPoint;
-				currentPoint.x += cubeStep;
-				xCount++;
-
-			}
-			currentPoint.y += cubeStep;
-			yCount++;
-		}
-		currentPoint.z += cubeStep;
-		zCount++;
-	}
-}*/
-
-void createCubeMesh(void) {
-	vec3_t vertices[NUM_VERTICES] = {
+bool createCubeMesh(void) {
+	 vec3_t vertices[NUM_VERTICES] = {
 		(vec3_t) {.x = -0.5, .y = -0.5, .z = 3}, // 0
 		(vec3_t) {.x = 0.5, .y = -0.5, .z = 3},  // 1
 		(vec3_t) {.x = 0.5, .y = 0.5, .z = 3},   // 2
@@ -83,13 +41,15 @@ void createCubeMesh(void) {
 		(face_t) {.a = 3, .b = 2, .c = 6}, // bottom
 		(face_t) {.a = 6, .b = 7, .c = 3},
 	};
-	intializeMesh(&mesh, NUM_VERTICES, NUM_FACES, vertices, faces);
+
+	return initializeMesh(&mesh, NUM_VERTICES, NUM_FACES, vertices, faces);
 }
 
 bool setup(void) {
 	// Creating raw pixel buffer
 	colorBuffer = (uint32_t *) malloc(sizeof(uint32_t) * windowWidth * windowHeight);
 	if (!colorBuffer) {
+		free(colorBuffer);
 		fprintf(stderr, "Failed to allocate memory for color buffer");
 		return false;
 	}
@@ -106,9 +66,8 @@ bool setup(void) {
 		SDL_Log("Unable to create color buffer texture %s", SDL_GetError());
 		return false;
 	}
-	
-	//createCubePointCloud();
-	return true;
+
+	return createCubeMesh();
 }
 
 void processInput(void) {
@@ -126,36 +85,14 @@ void processInput(void) {
 }
 
 void update(void) {
-	/*cubeRotation.x += 0.01;
-	cubeRotation.y += 0.01;
-	cubeRotation.z += 0.01;
-	for (int i = 0; i < CUBE_DIM * CUBE_DIM * CUBE_DIM; i++) {
-		vec3_t point = cubePointCloud[i];
-		point = addVec3(point, scaleVec3(-1.0, cubeCenter));
-		point = rotateVec3(point, cubeRotation.x, 'x');
-		point = rotateVec3(point, cubeRotation.y, 'y');
-		point = rotateVec3(point, cubeRotation.z, 'z');
-		point = addVec3(point, cubeCenter);
-		projectedPoints[i] = projectPoint(point);
-	}*/
-	rotateMesh(&mesh, 0.01, 'x');
-	rotateMesh(&mesh, 0.01, 'y');
-	rotateMesh(&mesh, 0.01, 'z');
+	//rotateMesh(&mesh, 0.01, 'y');
+	//rotateMesh(&mesh, 0.01, 'z');
 
 	int timeToWait = FRAME_TARGET_TIME - (SDL_GetTicks() - previousFrameTime);
 	if (timeToWait >0 && timeToWait <= FRAME_TARGET_TIME)
 		SDL_Delay(timeToWait);
 	previousFrameTime = SDL_GetTicks();
 }
-
-/*
-void drawCubePointCloud(void) {
-	for (int i = 0; i < CUBE_DIM * CUBE_DIM * CUBE_DIM; i++) {
-		pixel_t currentPixel = screenSpaceToPixelSpace(projectedPoints[i]);
-		drawRectangle(currentPixel.x - 2, currentPixel.y - 2, 4, 4,  0xFFFFFF00);
-	}
-	
-}*/
 
 void render(void) {
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
@@ -173,6 +110,7 @@ void render(void) {
 int main() {
 	isRunning = initializeWindow();
 	isRunning = setup();
+	rotateMesh(&mesh, 0.000, 'x');
 	while(isRunning) {
 		processInput();
 		update();	
--- src/mesh.h
+++ src/mesh.h
@@ -18,7 +18,7 @@ typedef struct {
 	vec3_t centroid;
 } mesh_t;
 
-void intializeMesh(mesh_t *mesh, int nVertices, int nFaces, vec3_t *vertices, face_t *faces); 
+bool initializeMesh(mesh_t *mesh, int nVertices, int nFaces, vec3_t *vertices, face_t *faces); 
 void drawMesh(mesh_t *mesh, uint32_t color);
 void destroyMesh(mesh_t *mesh);
 void rotateMesh(mesh_t *mesh, float theta, char axis);
